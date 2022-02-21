a4cf7543ed6dbb748a49ffc9b74c8f1e9100ea7b
Firgisotya <zzuuddiinn28@gmail.com>
2022-02-21 10:08:05 +0700

    Perubahan Kedua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index(){
+        return
+    '<h3>Nama : Firgi Sotya Izzuddin</h3>'.
+    '<h3>Nim  : 2041720207</h3>';
+    }
+}
